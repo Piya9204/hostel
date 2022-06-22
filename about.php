a3f0a206566049c5e37fc6a96211578ae07f674d
Alfiya Sayyad <alfiyasayyad903@gmail.com>
2022-06-22 22:30:27 +0530

    hostel website

---
+++ about.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About US</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+	
+	<style>
+		body{
+			background-image: url(3.png);
+			background-size: cover;
+			background-attachment: fixed;
+		}
+		
+.content{
+	
+	width: 50%
+	padding:80px;
+	margin: 150px auto;
+	margin-left: 500px;
+	margin-right: 50PX;
+	
+} 
+p{
+	font-size: 25px;
+	
+}
+
+
+	</style>
+
+</head>
+<body>
+	<div class="section">
+		<div class="container">
+			<div class="content-section">
+				<div class="title">
+					<h1>About Us</h1>
+
+					
+				</div>
+
+			<div class="content">
+				<h1>About Us</h1><h3>Hostel Managment System</h3>
+				<center><p align="center">As the name specifies “HOSTEL MANAGEMENT SYSTEM” is a software developed for managing various activities in the hostel. For the past few years the number of educational institutions are increasing rapidly. Thereby the number of hostels are also increasing for the accommodation of the students studying in this institution. And hence there is a lot of strain on the person who are running the hostel and software’s are not usually used in this context.</p></center>
+				<div class="button">
+					<a href="">Read more</a>
+					
+				</div>
+			</div>
+			
+			<div class="social">
+				<a href="https://www.linkedin.com/in/alfiya-sayyad-94b966218"><i class="fab fa-linkedin"></i><img src="3.pngss" im></a>
+				<a href="https://instagram.com/piya__9204?utm_medium=copy_link"><i class="fab fa-instagram"></i><p>instagram profile</p></a>		
+			</div>
+		
+			</div>
+			
+		</div>
+	
+	</div>
+
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>contact</title>
+
+
+
+	<style>
+		body{
+			background-image: url(images/1.jpg);
+			background-size: cover;
+			background-attachment: fixed;
+		}
+		p{
+	background: white;
+	width: 50%
+	padding:80px;
+	margin: 150px auto;
+	margin-left: 500px;
+	margin-right: 50PX;
+	
+} 
+p{
+	font-size: 25px;
+	color: black;
+	
+}
+
+	</style>
+</head>
+<body>
+	<p>
+		<b>If You Face any query or problem Feel free contact us</b>
+		<br>
+		<br>
+		<a href="alfiyasayyad903@gmail.com">Email</a>
+	</p>
+
+</body>
+</html>
